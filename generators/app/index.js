e0698c69b54d87f35d1cd5855fc9434ee91102a4
Pavlos Polianidis <ppoliani@gmail.com>
2017-07-13 13:15:47 +0100

    add templates and install deps to package

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,11 +7,83 @@ module.exports = class extends Generator {
     this.option('babel')
   }
 
-  method1() {
-    this.log('method 1 run');
+  initializing() {
+    this.log('Initializing');
   }
 
-  method2() {
-    this.log('method 2 run');
+  prompting() {
+    return this.prompt([
+      this._askProjectName(),
+      this._askReactVersion(),
+      this._askReduxVersion()
+    ])
+    .then(answers => {
+      this._reactVersion = answers.reactVersion;
+      this._reduxVersion = answers.reduxVersion;
+      this._projectName = answers.projectName;
+    });
+  }
+
+  configuring() {
+    this.log('configuring');
+  }
+
+  async asyncTask() {
+    this.log('asyncTask');
+
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log('async done');
+        resolve();
+      }, 2000);
+    })
+  }
+
+  defaultMethod() {
+    this.log('defaultMethod');
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('package.tpl'),
+      this.destinationPath('package.json'),
+      { name: this._projectName }
+    );
+  }
+
+  install() {
+    this.npmInstall([`react@${this._reactVersion}`], { 'save': true });
+    this.npmInstall(['redux'], { 'save': true });
+  }
+
+  end() {
+    this.log(`React Version: ${this._reactVersion}`);
+    this.log(`Redux Version: ${this._reduxVersion}`);
+  }
+
+  _askProjectName() {
+    return {
+      type: 'input',
+      name: 'projectName',
+      message: 'What is the name of the project'
+    };
+  }
+
+  _askReactVersion() {
+    return {
+      type: 'input',
+      name: 'reactVersion',
+      message: 'What version of React would you like to use',
+      default: '15'
+    };
+  }
+
+  _askReduxVersion() {
+    return {
+      type: 'input',
+      name: 'reduxVersion',
+      message: 'What version of Redux would you like to use',
+      default: '2.0.0'
+    };
   }
 }
